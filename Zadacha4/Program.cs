6d5712d9f22684e3f2a204f2ea782824fba85009
SkyLearn <ba_de@mail.ru>
2022-12-17 20:06:59 +0300

    Решено

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+Console.WriteLine($"Четные числа до этого числа:");
+while (index < N)
+{
+    index = index + 1;
+ 
+if (index%2==0)
+    Console.WriteLine(index);
+}
